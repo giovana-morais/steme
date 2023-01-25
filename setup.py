2e50b4a1e8c3dfa60a43caf9a460163f1eba6677
Giovana Morais <giovana.vmorais@gmail.com>
2023-01-25 15:56:43 -0300

    feat: create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+this file has setup to distribute this as a package
+"""
+
+from setuptools import setup
+
+setup(
+        name = "steme",
+        version = 0.1, 
+        description = "",
+        url = "",
+        author = "Giovana Morais",
+        author_email = "giovana.vmorais@gmail.com",
+        license = "",
+        # package_dir = {"steme": "src"},
+        packages = ["steme"],
+        zip_safe = False
+)
