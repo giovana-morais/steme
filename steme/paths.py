a5a2a4bd2706243dcd22600ff8a1167db7a0d964
Giovana Morais <giovana.vmorais@gmail.com>
2023-07-13 16:41:52 -0300

    refactor: remove hardcoded dataset path

--- steme/paths.py
+++ steme/paths.py
@@ -2,7 +2,7 @@ import os
 
 PROJECT_FOLDER = os.getcwd()
 DATA_FOLDER = os.path.join(PROJECT_FOLDER, "data")
-DATASET_FOLDER = os.path.join(PROJECT_FOLDER, "../datasets")
+DATASET_FOLDER = os.path.join(os.environ["HOME"], "datasets")
 FIG_FOLDER = os.path.join(PROJECT_FOLDER, "figures")
 LOG_FOLDER = os.path.join(PROJECT_FOLDER, "../logs")
 MODEL_FOLDER = os.path.join(PROJECT_FOLDER, "models")
